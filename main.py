06fbd2ebac9787525dc89c225923794eb93bae4b
devsangho <gaonmail@cau.ac.kr>
2023-08-28 17:58:18 +0900

    fix: use new main(not use threads when run models), add prints to gui program

--- main.py
+++ main.py
@@ -1,57 +1,117 @@
 import torch
 import cv2
-from worker import Worker
-from models.objectron.model import Objectron
-from models.hand_landmark_detection.model import HandLandmarkDetection
-from models.grasp_detection.model import GraspDetection
 import numpy as np
 
-from teensy import arduino1, arduino2
-from processings import hand, angle
-from sensors.imu import ImuAndEmg
+from utils.serial_reader import Serial
+from utils.video_capture import VideoCapture
 
-cap = cv2.VideoCapture(0)
+from models.objectron.model import Objectron
+from models.hand_landmark_detection.model import HandLandmarkDetection
+from sensors.imu_and_emg import IMUAndEMG
+from models.grasp_detection.model import GraspDetection
+
+from processings import hand, angle
+from config.teensy import serial_port1, serial_port2, serial_baudrate, timeout
+
+import time
+
+from queue import Queue
+from scipy.ndimage import gaussian_filter1d
+
 
 if __name__ == "__main__":
-    objectron = Objectron("Cup", cap)
-    objectron_thread = Worker(name="objectron", model=objectron)
+    ##################################################
+    ##                Communication                 ##
+    ##################################################
+    serial_imu_emg = Serial(serial_port1, is_reading=True)
+    serial_hand = Serial(serial_port2, is_reading=False)
 
-    hand_landmark_detection = HandLandmarkDetection(cap)
-    hand_landmark_detection_thread = Worker(
-        name="hand_landmark_detection", model=hand_landmark_detection
-    )
+    cap = VideoCapture(0)
+    # cap = cv2.VideoCapture(0)
 
-    imu_and_emg = ImuAndEmg()
-    imu_and_emg_thread = Worker(
-        name="imu_and_emg",
-        model=imu_and_emg,
-    )
+    # check VideoCaptureAPI
+    # 1400 MSMF (Microsoft Media Foundation)
+    print(cap.get(cv2.CAP_PROP_BACKEND))
+    # check camera FPS(not real FPS)
+    print(cap.get(cv2.CAP_PROP_FPS))
+
+    ##################################################
+    ##                    Model                     ##
+    ##################################################
+    objectron = Objectron("Cup")
+    hand_landmark_detection = HandLandmarkDetection()
+
+    imu_and_emg = IMUAndEMG()
 
     grasp_detection = GraspDetection(
         emglist_ch1=imu_and_emg.emglist_ch1,
         emglist_ch2=imu_and_emg.emglist_ch2,
     )
-    grasp_detection_thread = Worker(name="grasp_detection", model=grasp_detection)
 
-    arduino1_thread = Worker(name="arduino1", model=arduino1)
-    arduino2_thread = Worker(name="arduino2", model=arduino2)
+    ##################################################
+    ##                     GUI                      ##
+    ##################################################
+    winname = "auto-control-prosthetic-hand system"
+    display_width = 16 * 50
+    display_height = 9 * 50
+    cv2.namedWindow(winname)
+    cv2.resizeWindow(winname=winname, width=display_width * 2, height=display_height)
+    cv2.moveWindow(winname=winname, x=0, y=0)
 
-    arduino1_thread.start()
-    arduino2_thread.start()
-    objectron_thread.start()
-    hand_landmark_detection_thread.start()
-    imu_and_emg_thread.start()
-    grasp_detection_thread.start()
+    serial_imu_emg.write("start!")
+
+    angles_max_length = 200
+    angles = []
+    hand_angle = None
+    estimated_angle = None
+    hand_position = None
 
     with torch.no_grad():
-        while cap.isOpened():
-            if objectron.image is None or hand_landmark_detection.image is None:
-                continue
+        if not cap.isOpened():
+            print("Cannot open camera")
+            exit()
+        while True:
+            # task 1(500Hz)
+            # read imu & emg data
+
+            # task 2(~100ms loop)
+            # capture frame > apply model > write calculated data
+
+            start_time = time.time()
+            ##################################################
+            ##                Capture frame                 ##
+            ##################################################
+            # Capture frame-by-frame
+            ret, frame = cap.retrive()
+
+            # # Wait until next frame and Grab
+            # # if processing time is over real camera FPS, becomes zero
+            # cap.grab()
+            # # Decode grabbed frame
+            # ret, frame = cap.retrieve()
+
+            # if frame is read correctly ret is True
+            if not ret:
+                # print("Can't receive frame (stream end?). Exiting ...")
+                print("Can't retrive frame (stream end?). Exiting ...")
+                break
+
+            ##################################################
+            ##                  Processing                  ##
+            ##################################################
+            imu_and_emg.buffer_to_data(serial_imu_emg.buffer_str)
+
+            objectron.run(frame)
+            hand_landmark_detection.run(frame)
+            grasp_detection.run()
 
             head_angle = None
+            write_buffer_str = ""
+
             hand_position = hand.get_position(
                 hand_landmark_detection.landmarks, objectron.landmarks_3d
             )
+            write_buffer_str += hand_position
 
             if (
                 objectron.rotation_matrix is not None
@@ -60,41 +120,82 @@ if __name__ == "__main__":
                 head_angle = angle.get_head_angle(
                     objectron.rotation_matrix, imu_and_emg.rotation_matrix
                 )
-            if head_angle is not None and objectron.landmarks_3d is not None:
                 hand_angle = angle.get_hand_angle(head_angle)
-                arduino2.send(hand_angle)
+                angles.append(hand_angle)
 
             if grasp_detection.y_pred is not None:
-                if int(grasp_detection.y_pred) == 0 or int(grasp_detection.y_pred) == 2:
-                    arduino2.send("v")
+                if int(grasp_detection.y_pred) == 0:
+                    pass
                 elif int(grasp_detection.y_pred) == 1:
-                    arduino2.send("c")
+                    write_buffer_str += "grasp"
+                    pass
+                elif int(grasp_detection.y_pred) == 2:
+                    write_buffer_str += "release"
+                    pass
 
-            arduino2.send(hand_position)
+            if len(angles) >= 1:
+                estimated_angle = gaussian_filter1d(angles, sigma=20)[-1]
+                write_buffer_str += "/%03d" % (estimated_angle)
 
-            horizontal_images = np.hstack(
+            if write_buffer_str != "":
+                serial_hand.write(write_buffer_str)
+
+            ##################################################
+            ##                     GUI                      ##
+            ##################################################
+            # Add serial oscilloscope?
+
+            horizontal_frames = np.hstack(
                 [objectron.image, hand_landmark_detection.image]
             )
+            horizontal_frames = cv2.resize(
+                horizontal_frames,
+                (display_width * 2, display_height),
+                interpolation=cv2.INTER_AREA,
+            )
 
+            is_grasping = grasp_detection.y_pred
             text = (
-                "angle: " + str(head_angle) if head_angle is not None else "loading..."
+                "is_grasping: " + str(is_grasping) + "   "
+                if is_grasping is not None
+                else "is_grasping: loading...   "
             )
-            x, y, w, h = 0, 0, 700, 75
-            cv2.rectangle(horizontal_images, (x, x), (x + w, y + h), (0, 0, 0), -1)
+            text = (
+                text + "hand_angle: " + str(estimated_angle) + "   "
+                if estimated_angle is not None
+                else text + "hand_angle: loading...   "
+            )
+            text = (
+                text + "hand position: " + str(hand_position)
+                if hand_position is not None
+                else text + "hand_position: loading..."
+            )
+            w, h = 2000, 50
+            cv2.rectangle(
+                img=horizontal_frames,
+                pt1=(0, 0),
+                pt2=(w, h),
+                color=(0, 0, 0),
+                thickness=-1,
+            )
             cv2.putText(
-                horizontal_images,
-                text,
-                (x + int(w / 10), y + int(h / 2)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
+                img=horizontal_frames,
+                text=text,
+                org=(int(w / 50), int(h / 1.5)),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1,
+                color=(255, 255, 255),
+                thickness=1,
             )
 
-            cv2.imshow("auto-prothetic-hand control system", horizontal_images)
+            cv2.imshow("auto-control-prosthetic-hand system", horizontal_frames)
 
-            if cv2.waitKey(10) & 0xFF == ord("q"):
+            # cv2.pollKey()
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
+
+            end_time = time.time()
+            print("total loop: %dms" % ((end_time - start_time) * 1e3))
 
     cap.release()
     cv2.destroyAllWindows()
